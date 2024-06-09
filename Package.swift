357715a02c417bfb5ae9649666201f36c3064dc4
Vatsal Manot <vatsal.manot@yahoo.com>
2024-06-09 02:30:19 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vmanot/CorePersistence.git", branch: "main"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", exact: "2.6.0"),
+        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
+        .package(url: "https://github.com/vmanot/CorePersistence.git", branch: "main"),
         .package(url: "https://github.com/vmanot/NetworkKit.git", branch: "master"),
         .package(url: "https://github.com/vmanot/Swallow.git", branch: "master"),
-        .package(url: "https://github.com/vmanot/SwiftUIX.git", branch: "master"),
     ],
     targets: [
         .target(
